1dcaaedb1e3b604dc024fc9d6051a2fa27d24325
Demel9n <playboy41k@i.ua>
2021-02-27 19:27:32 +0300

    Add reusable possibility

--- LT/Program.cs
+++ LT/Program.cs
@@ -7,36 +7,45 @@ namespace LT
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите номер трамвайного билета:  ");
-            string str = Console.ReadLine();
-            char[] ch = new char[str.Length];
-            ch = str.ToCharArray();
-            int[] ticketNumber = ch.Select(s => int.Parse(s.ToString())).ToArray();
-            if (ch.Length>3 && ch.Length<9)
+            do
             {
-                int leftNumber = 0;
-                int rightNumber = 0;
-                for (int i = 0; i < ch.Length; i++)
+                Console.Clear();
+                Console.Write("Введите номер трамвайного билета:  ");
+                string str = Console.ReadLine();
+                char[] ch = new char[str.Length];
+                ch = str.ToCharArray();
+                int[] ticketNumber = ch.Select(s => int.Parse(s.ToString())).ToArray();
+                if (ch.Length > 3 && ch.Length < 9)
                 {
-                    if (i < ch.Length/2)
+                    int leftNumber = 0;
+                    int rightNumber = 0;
+                    for (int i = 0; i < ch.Length; i++)
                     {
-                        leftNumber += ticketNumber[i];
+                        if (i < ch.Length / 2)
+                        {
+                            leftNumber += ticketNumber[i];
+                        }
+
+                        else rightNumber += ticketNumber[i];
                     }
 
-                    else rightNumber += ticketNumber[i];
+                    if (leftNumber == rightNumber)
+                    {
+                        Console.WriteLine("У вас счастливый билет: {0} = {1}", leftNumber, rightNumber);
+                    }
+
+                    else Console.WriteLine("У вас не счастливый билет: {0} != {1}", leftNumber, rightNumber);
                 }
 
-                if (leftNumber == rightNumber)
+                else
                 {
-                    Console.WriteLine("У вас счастливый билет: {0} = {1}", leftNumber, rightNumber);
+                    Console.WriteLine("Число введено не правильно!");
                 }
-
-                else Console.WriteLine("У вас не счастливый билет: {0} != {1}", leftNumber, rightNumber);
-            }
-
-            else Console.WriteLine("Число введено не правильно!");
-
-            Console.Read();
+                Console.ReadKey();
+                Console.Clear();
+                Console.WriteLine("Если желаете выйти нажмите  Y ");
+                Console.WriteLine("Если желаете продолжить нажмите любую другую кнопку ");
+            } while (Console.ReadLine() != "Y");
         }
     }
 }
